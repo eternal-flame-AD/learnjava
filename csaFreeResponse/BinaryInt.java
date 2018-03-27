286e1ec549e7aa3d7a1fcd08bdf8422363ea74b8
_eternal_flame <35571479+eternal-flame-AD@users.noreply.github.com>
2018-03-27 21:01:16 +0800

    模考2 Binary Bug Fix

--- csaFreeResponse/BinaryInt.java
+++ csaFreeResponse/BinaryInt.java
@@ -24,6 +24,61 @@ public class BinaryInt implements Comparable<BinaryInt> {
 				result="0";
 		return result;
 	}
+	private boolean addDigit(boolean a,boolean b,int pos,boolean add_num) {
+		if (add_num) {
+			if (a&&b) {
+				this.binaryNumber.set(pos, true);
+				return true;
+			} else if (a||b) {
+				this.binaryNumber.set(pos, false);
+				return true;
+			} else {
+				this.binaryNumber.set(pos, true);
+				return false;
+			}
+		}
+		if (a^b) {
+			this.binaryNumber.set(pos, true);
+			return false;
+		} else {
+			this.binaryNumber.set(pos, false);
+			return (a&&b);
+		}
+	}
+	public BinaryInt add(BinaryInt oper) {
+		int len1=this.binaryNumber.size();
+		int len2=oper.binaryNumber.size();
+		if (len1<len2) {
+			int i;
+			boolean add_num=false;
+			for (i=len1-1;i>=0;i--) {
+				add_num=addDigit(this.binaryNumber.get(i),oper.binaryNumber.get(i+len2-len1),i,add_num);
+			}
+			for (i=len2-len1-1;i>=0;i--) {
+				this.binaryNumber.add(0,oper.binaryNumber.get(i));
+			}
+		} else {
+			int i;
+			boolean add_num=false;
+			for (i=len2-1;i>=0;i--) {
+				add_num=addDigit(this.binaryNumber.get(i+len1-len2),oper.binaryNumber.get(i),i+len1-len2,add_num);
+			}
+			i=i+len1-len2;
+			while ((add_num) && (i>=0)) {
+				add_num=addDigit(this.binaryNumber.get(i),false,i,add_num);
+				i--;
+			}
+			if (add_num)
+				this.binaryNumber.add(0, true);
+		}
+		return this;
+	}
+	public void dump() {
+		for (boolean digit:this.binaryNumber) {
+			System.out.print(digit?1:0);
+		}
+		System.out.println();
+	}
 	public int compareTo(BinaryInt oper) {
 		// TODO Auto-generated method stub
 		if (this.binaryNumber.size()==oper.binaryNumber.size()) {
