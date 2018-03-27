e082d7f2524434ebaf577a6f90ea07fda6924562
_eternal_flame <35571479+eternal-flame-AD@users.noreply.github.com>
2018-03-27 22:40:02 +0800

    Remove debug info

--- csaFreeResponse/TestBinaryInt.java
+++ csaFreeResponse/TestBinaryInt.java
@@ -3,12 +3,9 @@ package csaFreeResponse;
 public class TestBinaryInt {
 	public static void main(String[] args) {
 		BinaryInt bin1=new BinaryInt(127);
-		bin1.dump();
 		BinaryInt one=new BinaryInt(1);
-		one.dump();
 		BinaryInt bin2=new BinaryInt(128);
 		bin1.add(one);
-		bin1.dump();
 		System.out.println(bin1.compareTo(bin2));
 	}
 }
