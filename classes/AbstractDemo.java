0ee1ea2c3ea11be9f2795e5ea30aae64223485fb
_eternal_flame <35571479+eternal-flame-AD@users.noreply.github.com>
2018-03-27 11:01:17 +0800

    Add constructor

--- classes/AbstractDemo.java
+++ classes/AbstractDemo.java
@@ -3,6 +3,9 @@ abstract class book {
 	public String ISBN;
 	public String title;
 	public int pagenow=1;
+	protected book() { //constructor dont have return types
+		this.pagenow=100; 
+	}
 	public void flip_page() {
 		pagenow++;
 		System.out.println("Flipped to page "+pagenow);
@@ -13,7 +16,7 @@ class AP_Princeton extends book {
 	public String title="Cracking the AP CSA Exam";
 	private static String Customer_review;
 	public void flip_page() {
-		System.out.println("FLiiping page of:"+this.title);
+		System.out.println("FLipping page of:"+this.title);
 		super.flip_page();  //Not this.super
 	}
 	public void write_review(String rev) {
@@ -30,7 +33,7 @@ public class AbstractDemo {
 		myAPbook.flip_page();
 		/*
 		 * FLiiping page of:Cracking the AP CSA Exam
-		 * Flipped to page 2
+		 * Flipped to page 101
 		 */
 		((AP_Princeton)myAPbook).write_review("Sucks..."); //explicitly tell java this is an instance of AP_Princeton or it will NOT compile
 		AP_Princeton anotherAPbook=new AP_Princeton();
